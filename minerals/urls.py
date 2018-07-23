f1e2cbafb074af5143a9e281f1a680dba1ea637e
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-07-23 15:00:46 -0400

    Changed file

--- minerals/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-	url(r'^index/$', views.index),
-	url(r'detail(?P<pk>\d+)/$', views.detail, name='detail'),
-	url(r'detail(?P<mineral_pk>\d+)/$', views.detail, name='detail'),
-]
