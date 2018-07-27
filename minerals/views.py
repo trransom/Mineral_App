896158ea9aec1eeaa0fc51da4461ce441dffa443
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-07-27 09:50:51 -0400

    Added home url

--- minerals/views.py
+++ minerals/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Mineral
 
+def home(request):
+	return render(request, 'minerals/home.html')
+
 def index(request):
 	minerals = Mineral.objects.all()
 	return render(request, 'minerals/index.html', {'minerals': minerals})
 	
 def detail(request, pk):
-#	mineral = Mineral.objects.get(pk=pk)
 	mineral = get_object_or_404(Mineral, pk=pk)
 	return render(request, 'minerals/mineral_detail.html', {'mineral': mineral})
